d9079042306b26a29ec224b5cdf5f68b4f7052bf
ribsthakkar <rishabh.thakkar@gmail.com>
2018-09-06 18:18:54 -0500

    First workshop materials

---
+++ Flashlight/Flashlight/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Rishabh Thakkar on 9/6/18.
+//  Copyright © 2018 ribsthak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var isLightOn:Bool = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setNeedsStatusBarAppearanceUpdate()
+        Toggle.setTitle("Off", for: .normal)
+        Toggle.setTitleColor(.black, for: .normal)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if isLightOn {
+            return .default
+        } else {
+            return .lightContent
+        }
+    }
+    
+    @IBOutlet weak var Toggle: UIButton!
+    
+    @IBAction func toggleLight(_ sender: Any) {
+        if isLightOn {
+            view.backgroundColor = .black
+            Toggle.setTitleColor(.white, for: .normal)
+            Toggle.setTitle("On", for: .normal)
+            isLightOn = false
+            print(preferredStatusBarStyle)
+        } else {
+            view.backgroundColor = .white
+            Toggle.setTitleColor(.black, for: .normal)
+            Toggle.setTitle("Off", for: .normal)
+            isLightOn = true
+            print(preferredStatusBarStyle)
+        }
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+}
+
